be5d0ed5d9fd4b63eff1f3b470b2faa0e83206e7
Alexx-Viv <mario.alex.vivsenco@gmail.com>
2021-06-30 14:09:47 +0200

    Finish

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var name = prompt("Inserire il proprio nome");
+var surname = prompt("Inserire il proprio cognome");
+var color = prompt("Inserire il proprio colore preferito");
+
+document.getElementById("password").innerText = "La tua password ultra-segreta è: " + name + surname + color + "21";
